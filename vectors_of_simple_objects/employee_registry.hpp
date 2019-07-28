44f9d7ac7122c6b06e42cf7e84d96fddd32c1393
AlexCombat <combat0705@yandex.ru>
2019-07-28 22:12:26 +0300

    Use vectors of simple object instead of complex objects

---
+++ vectors_of_simple_objects/employee_registry.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "employee.hpp"
+#include "filter.hpp"
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <functional>
+
+namespace filtering
+{
+    class IEmployeeRegistry
+    {
+    public:
+        using Ptr = std::unique_ptr<IEmployeeRegistry>;
+
+        static Ptr load(std::string file);
+
+        virtual std::vector<size_t> filter(
+            IFilter<std::string>::Ptr nameFilter,
+            IFilter<std::string>::Ptr positionFilter,
+            IFilter<int>::Ptr ageFilter,
+            IFilter<float>::Ptr salaryFilter) const = 0;
+        virtual void add(Employee employee) = 0;
+    };
+}
---
+++ vectors_of_simple_objects/filter.hpp
@@ -0,0 +1,149 @@
+#pragma once
+
+#include <type_traits>
+#include <memory>
+#include <vector>
+
+namespace filtering
+{
+    template<class T, class Filter>
+    void match(const std::vector<T>& values, std::vector<bool>& result, Filter filter)
+    {
+//        int mainLoop = values.size() / 8;
+//#pragma ivdep
+//#pragma vector always
+//        for (int i = 0; i < mainLoop * 8; i++)
+//        {
+//            result[i] = result[i] & filter(values[i]);
+//        }
+//
+//        for (int i = mainLoop * 8; i < values.size(); i++)
+//        {
+//            result[i] = result[i] & filter(values[i]);
+//        }
+
+        for (int i = 0; i < values.size(); i++)
+        {
+            result[i] = result[i] & filter(values[i]);
+        }
+    }
+
+    template<class T>
+    class IFilter
+    {
+    public:
+        using Ptr = std::shared_ptr<IFilter>;
+
+        virtual ~IFilter() = default;
+
+        virtual void match(const std::vector<T>& values, std::vector<bool>& result) const = 0;
+    };
+
+    template<class T>
+    class ISingleValueFilter : public IFilter<T>
+    {
+    public:
+        ISingleValueFilter(const T& value)
+            : IFilter(), m_value{ value } {}
+
+    protected:
+        T m_value;
+    };
+
+    template<class T>
+    class EqualsFilter final : public ISingleValueFilter<T>
+    {
+    public:
+        EqualsFilter(const T& value)
+            : ISingleValueFilter(value) {}
+
+        void match(const std::vector<T>& values, std::vector<bool>& result) const override
+        {
+            filtering::match(values, result, [this](auto value)
+            {
+                return value == m_value;
+            });
+        }
+    };
+
+    template<class T>
+    class NotEqualsFilter final : public ISingleValueFilter<T>
+    {
+    public:
+        NotEqualsFilter(const T& value)
+            : ISingleValueFilter(value) {}
+
+        void match(const std::vector<T>& values, std::vector<bool>& result) const override
+        {
+            filtering::match(values, result, [this](auto value)
+            {
+                return value != m_value;
+            });
+        }
+    };
+
+    template<class T>
+    class GreaterFilter final : public ISingleValueFilter<T>
+    {
+    public:
+        GreaterFilter(const T& value)
+            : ISingleValueFilter(value) {}
+
+        void match(const std::vector<T>& values, std::vector<bool>& result) const override
+        {
+            filtering::match(values, result, [this](auto value)
+            {
+                return value > m_value;
+            });
+        }
+    };
+
+    template<class T>
+    class GreaterOrEqualsFilter final : public ISingleValueFilter<T>
+    {
+    public:
+        GreaterOrEqualsFilter(const T& value)
+            : ISingleValueFilter(value) {}
+
+        void match(const std::vector<T>& values, std::vector<bool>& result) const override
+        {
+            filtering::match(values, result, [this](auto value)
+            {
+                return value >= m_value;
+            });
+        }
+    };
+
+    template<class T>
+    class LessFilter final : public ISingleValueFilter<T>
+    {
+    public:
+        LessFilter(const T& value)
+            : ISingleValueFilter(value) {}
+
+        void match(const std::vector<T>& values, std::vector<bool>& result) const override
+        {
+            filtering::match(values, result, [this](auto value)
+            {
+                return value < m_value;
+            });
+        }
+    };
+
+    template<class T>
+    class LessOrEqualsFilter final : public ISingleValueFilter<T>
+    {
+    public:
+        LessOrEqualsFilter(const T& value)
+            : ISingleValueFilter(value) {}
+
+        void match(const std::vector<T>& values, std::vector<bool>& result) const override
+        {
+            filtering::match(values, result, [this](auto value)
+            {
+                return value <= m_value;
+            });
+        }
+    };
+
+}
