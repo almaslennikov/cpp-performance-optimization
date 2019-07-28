9a9753394bd4de2b77f3d2d8706533b85c0f4901
AlexCombat <combat0705@yandex.ru>
2019-07-28 22:12:26 +0300

    Use vectors of simple object instead of complex objects
     Please enter the commit message for your changes. Lines starting

---
+++ vectors_of_simple_objects/employee_registry.cpp
@@ -0,0 +1,115 @@
+#include "employee_registry.hpp"
+
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+
+namespace filtering
+{
+    class EmployeeRegistry : public IEmployeeRegistry
+    {
+    public:
+        EmployeeRegistry(std::string file);
+
+        std::vector<size_t> filter(
+            IFilter<std::string>::Ptr nameFilter,
+            IFilter<std::string>::Ptr positionFilter,
+            IFilter<int>::Ptr ageFilter,
+            IFilter<float>::Ptr salaryFilter) const override;
+        void add(Employee employee) override;
+
+    private:
+        std::vector<std::string> m_names;
+        std::vector<std::string> m_positions;
+        std::vector<int> m_ages;
+        std::vector<float> m_salaries;
+    };
+
+    IEmployeeRegistry::Ptr IEmployeeRegistry::load(std::string file)
+    {
+        return std::make_unique<EmployeeRegistry>(std::move(file));
+    }
+
+    EmployeeRegistry::EmployeeRegistry(std::string file)
+    {
+        std::ifstream csv(file);
+
+        for (std::string line; std::getline(csv, line);)
+        {
+            std::stringstream lineStream(line);
+            std::string cell;
+
+            std::string name;
+            std::string position;
+            int age;
+            float salary;
+
+            std::getline(lineStream, cell, ',');
+            name = std::move(cell);
+
+            std::getline(lineStream, cell, ',');
+            position = std::move(cell);
+
+            std::getline(lineStream, cell, ',');
+            age = std::stoi(std::move(cell));
+
+            std::getline(lineStream, cell, ',');
+            salary = std::stof(std::move(cell));
+
+            m_names.emplace_back(std::move(name));
+            m_positions.emplace_back(std::move(position));
+            m_ages.emplace_back(age);
+            m_salaries.emplace_back(salary);
+        }
+    }
+
+    std::vector<size_t> EmployeeRegistry::filter(
+        IFilter<std::string>::Ptr nameFilter,
+        IFilter<std::string>::Ptr positionFilter,
+        IFilter<int>::Ptr ageFilter,
+        IFilter<float>::Ptr salaryFilter) const
+    {
+        std::vector<bool> matched(m_names.size(), true);
+
+        if (nameFilter)
+        {
+            nameFilter->match(m_names, matched);
+        }
+
+        if (positionFilter)
+        {
+            positionFilter->match(m_positions, matched);
+        }
+
+        if (ageFilter)
+        {
+            ageFilter->match(m_ages, matched);
+        }
+
+        if (salaryFilter)
+        {
+            salaryFilter->match(m_salaries, matched);
+        }
+
+        std::vector<size_t> result;
+        result.reserve(matched.size());
+
+        for (size_t i = 0; i < m_names.size(); i++)
+        {
+            if (matched[i])
+            {
+                result.push_back(i);
+            }
+        }
+
+        return result;
+    }
+
+    void EmployeeRegistry::add(Employee employee)
+    {
+        m_names.emplace_back(std::move(employee.name));
+        m_positions.emplace_back(std::move(employee.position));
+        m_ages.emplace_back(employee.age);
+        m_salaries.emplace_back(employee.salary);
+    }
+}
